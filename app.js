8666f6e6de374bf510a58eec4c5300c59978cc2d
Vijay Kumar Gupta <guptavinay225@gmail.com>
2022-07-06 07:31:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,111 @@
+//jshint esversion:6
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-Parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
+
+const app = express();
+
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+mongoose.connect("mongodb://localhost:27017/expynoseDB");
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.get("/", function(req, res) {
+  res.render("home")
+});
+
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+app.post("/register", function(req, res) {
+
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    // Store hash in your password DB.
+
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets")
+      }
+    });
+
+
+
+  });
+
+
+
+
+
+
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          // result == true
+          if (result === true) {
+            res.render("secrets")
+
+          }
+        });
+      }
+    }
+  });
+});
+
+app.get("/logout", function(req, res) {
+  res.render("home");
+})
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function() {
+  console.log("Server is active");
+});
